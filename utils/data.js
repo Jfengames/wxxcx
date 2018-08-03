b18177e4411f4b2282ed6e3ac687f393633a8f67
feng wei <fengwei411@126.com>
2018-08-03 19:41:44 +0800

    微信小程序-答题开发第一次上传

---
+++ utils/data.js
@@ -0,0 +1,347 @@
+const python = [
+  {
+    "id": 1,
+    "type": "单选",
+    "q": " 当UE发起异频切换时，测量GAP有两种模式，模式1Tgap为6ms，周期为40ms，模式2Tgap为6ms，周期为（）",
+    "options": [
+      {
+        "label": "A",
+        "text": "60ms"
+      },
+      {
+        "label": "B",
+        "text": "80ms"
+      },
+      {
+        "label": "C",
+        "text": "20ms"
+      },
+      {
+        "label": "D",
+        "text": "10ms"
+      }
+    ],
+    "a": "B",
+    "isStore": "false",
+    "isAnswer": 0
+  },
+  {
+    "id": 2,
+    "type": "单选",
+    "q": " 仅考虑业务量，以下扩容优先级最低的小区是（）",
+    "options": [
+      {
+        "label": "A",
+        "text": "忙时平均在线用户数150"
+      },
+      {
+        "label": "B",
+        "text": "忙时PRB平均利用率80%"
+      },
+      {
+        "label": "C",
+        "text": "忙时上行平均流速5M"
+      },
+      {
+        "label": "D",
+        "text": "忙时下行平均吞吐率10Mbps"
+      }
+    ],
+    "a": "D",
+    "isStore": "false",
+    "isAnswer": 0
+  },
+  {
+    "id": 3,
+    "type": "单选",
+    "q": "___反映系统的通讯保持能力",
+    "options": [
+      {
+        "label": "A",
+        "text": "E-RAB建立阻塞率"
+      },
+      {
+        "label": "B",
+        "text": "切换成功率"
+      },
+      {
+        "label": "C",
+        "text": "E-RAB掉话率"
+      },
+      {
+        "label": "D",
+        "text": "RRC重建成功率"
+      }
+    ],
+    "a": "C",
+    "isStore": "false",
+    "isAnswer": 0
+  },
+  {
+    "id": 4,
+    "type": "单选",
+    "q": "___反映小区对UE呼叫的接纳能力，直接影响用户对网络使用的感受:",
+    "options": [
+      {
+        "label": "A",
+        "text": "无线接通率"
+      },
+      {
+        "label": "B",
+        "text": "RRC连接建立成功率（业务相关）"
+      },
+      {
+        "label": "C",
+        "text": "E-RAB建立阻塞率"
+      },
+      {
+        "label": "D",
+        "text": "E-RAB建立成功率"
+      }
+    ],
+    "a": "A",
+    "isStore": "false",
+    "isAnswer": 0
+  },
+  {
+    "id": 5,
+    "type": "单选",
+    "q": "65、 LTE传输块的信道编码方案采用Turbo编码，编码速率为:",
+    "options": [
+      {
+        "label": "A",
+        "text": "R=1"
+      },
+      {
+        "label": "B",
+        "text": "R=1/2"
+      },
+      {
+        "label": "C",
+        "text": "R=1/3"
+      },
+      {
+        "label": "D",
+        "text": "R=1/5"
+      }
+    ],
+    "a": "C",
+    "isStore": "false",
+    "isAnswer": 0
+  },
+  {
+    "id": 6,
+    "type": "单选",
+    "q": "ATTACH REQUEST, ATTACH ACCEPT分别包含于哪条空口RRC消息内:（）",
+    "options": [
+      {
+        "label": "A",
+        "text": "RRC CONNECTION REQUEST, RRC CONNECTION SETUP"
+      },
+      {
+        "label": "B",
+        "text": "RRC CONNECTION SETUP, RRC CONNECTION SETUP COMPELTE"
+      },
+      {
+        "label": "C",
+        "text": "RRC CONNECTION SETUP COMPELTE,RRC CONNECTION RECONFIGURATION"
+      },
+      {
+        "label": "D",
+        "text": "RRC CONNECTION RECONFIGURATION, RRC CONNECTION SETUP RECONFIGURATION COMPELTE"
+      }
+    ],
+    "a": "C",
+    "isStore": "false",
+    "isAnswer": 0
+  },
+  {
+    "id": 7,
+    "type": "单选",
+    "q": "DwPTS用于下行数据发送，其长度最多可以是（）个OFDM符号？",
+    "options": [
+      {
+        "label": "A",
+        "text": "8"
+      },
+      {
+        "label": "B",
+        "text": "10"
+      },
+      {
+        "label": "C",
+        "text": "11"
+      },
+      {
+        "label": "D",
+        "text": "12"
+      }
+    ],
+    "a": "D",
+    "isStore": "false",
+    "isAnswer": 0
+  },
+  {
+    "id": 8,
+    "type": "单选",
+    "q": "EIRP是下列（）端口的发射功率",
+    "options": [
+      {
+        "label": "A",
+        "text": "RRU口"
+      },
+      {
+        "label": "B",
+        "text": "天线口"
+      },
+      {
+        "label": "C",
+        "text": "机顶口"
+      },
+      {
+        "label": "D",
+        "text": "馈线口"
+      }
+    ],
+    "a": "B",
+    "isStore": "false",
+    "isAnswer": 0
+  },
+  {
+    "id": 9,
+    "type": "单选",
+    "q": "F频段站点，SA=2，SSP=5时，若PRACH采用格式4，PRACH在什么位置进行传输",
+    "options": [
+      {
+        "label": "A",
+        "text": "DwPTS"
+      },
+      {
+        "label": "B",
+        "text": "UpPTS"
+      },
+      {
+        "label": "C",
+        "text": "UL子帧"
+      },
+      {
+        "label": "D",
+        "text": "DL子帧"
+      }
+    ],
+    "a": "B",
+    "isStore": "false",
+    "isAnswer": 0
+  },
+  {
+    "id": 10,
+    "type": "单选",
+    "q": "II技术是用来解决？（  ）",
+    "options": [
+      {
+        "label": "A",
+        "text": "邻频干扰"
+      },
+      {
+        "label": "B",
+        "text": "同频干扰"
+      },
+      {
+        "label": "C",
+        "text": "随机干扰"
+      },
+      {
+        "label": "D",
+        "text": "异系统干扰"
+      }
+    ],
+    "a": "B",
+    "isStore": "false",
+    "isAnswer": 0
+  },
+
+  
+  {
+    "id": 11,
+    "type": "多选",
+    "q": "CQI可以由下面哪几个信道进行传送?",
+    "options": [
+      {
+        "label": "A",
+        "text": "PRACH"
+      },
+      {
+        "label": "B",
+        "text": "PUCCH"
+      },
+      {
+        "label": "C",
+        "text": "UE-specific RS"
+      },
+      {
+        "label": "D",
+        "text": "PUSCH"
+      }
+    ],
+    "a": "BD",
+    "isStore": "false",
+    "isAnswer": 0
+  },
+  {
+    "id": 12,
+    "type": "多选",
+    "q": "EPS 移动性管理状态包括",
+    "options": [
+      {
+        "label": "A",
+        "text": "EMM-REGISTERED"
+      },
+      {
+        "label": "B",
+        "text": "EMM-DEREGISTERED"
+      },
+      {
+        "label": "C",
+        "text": "ECM-IDLE"
+      },
+      {
+        "label": "D",
+        "text": "ECM-CONNECTED"
+      }
+    ],
+    "a": "AB",
+    "isStore": "false",
+    "isAnswer": 0
+  },
+  {
+    "id": 13,
+    "type": "多选",
+    "q": "E-RABQoS等级参数包含:",
+    "options": [
+      {
+        "label": "A",
+        "text": "QCI"
+      },
+      {
+        "label": "B",
+        "text": "GBRQoSInformation"
+      },
+      {
+        "label": "C",
+        "text": "CQI"
+      },
+      {
+        "label": "D",
+        "text": "ARP"
+      }
+    ],
+    "a": "ABD",
+    "isStore": "false",
+    "isAnswer": 0
+  }
+]
+
+
+module.exports = {
+  data: python
+}
